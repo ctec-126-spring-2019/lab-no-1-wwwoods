b88bd7d24b0a1fd91905bbe464dccbfdd0a22c58
wwwoods <w.woods3@students.clark.edu>
2019-04-21 22:57:19 -0700

    Assignment Completed

--- js/lab1.js
+++ js/lab1.js
@@ -5,7 +5,8 @@ function reverseString(str) {
 }
 
 function listSkills(){
-    let skills = ['Web Development','Gardening','Drumming','Volunteering';
+    //add bracket to line 8 to close array
+    let skills = ['Web Development','Gardening','Drumming','Volunteering'];
     let output = "<h2>Bruce's Skills<h2>";
     output += '<ul>';
     for (let i = 0; i < skills.length; i++) {
@@ -16,15 +17,18 @@ function listSkills(){
 }
 
 function getStarted(){
-    let firstName  'Bruce'
+    //Lines 20 and 22 need semicolons. Line 21 needs =, Add dollar signs to declare last name, city and gpa.
+    let firstName = 'Bruce';
     let lastName = 'Elgort';
-    let collegeName = 'Clark College'
+    let collegeName = 'Clark College';
     let city = 'Vancouver, Washington';
     let gpa = 3.2;
-    let output = `${firstName} {lastName} is currently attending ${collegeName}, located in {city} and has a grade point average of {gpa}`;
-
-    document.getElementById('place1').innerHTML = '<p>' + output + '</p>";
-    document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
+    let output = `${firstName} ${lastName} is currently attending ${collegeName}, located in ${city} and has a grade point average of ${gpa}`;
+    // incorrect apostrophe line 28.
+    document.getElementById('place1').innerHTML = '<p>' + output + '</p>';
+    //remove redundent lines 30 & 31.
+    //document.getElementById('place2').innerHTML = '<p>' + output + '</p>';
+    //document.getElementById('place2').innerHTML = '<p>' + reverseString(output) + '</p>';
 }
 
 window.onload = function(){
